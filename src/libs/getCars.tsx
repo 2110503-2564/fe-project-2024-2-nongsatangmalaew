38e29053c0d5bded7111af83d2d4fc22ec0129de
tahphalat <phalat.tah@gmail.com>
2025-03-24 21:59:52 +0700

    vercel2

--- src/libs/getCars.tsx
+++ src/libs/getCars.tsx
@@ -8,6 +8,5 @@ export default async function getCars():Promise<CarsJson>{
     if(!response.ok){
         throw new Error("Failed to fetch cars");
     }
-    
     return await response.json() as CarsJson;
 }
