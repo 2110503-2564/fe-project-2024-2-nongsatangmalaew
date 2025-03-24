800d240a2f7218795b808cb31cbb34c4b4f43cbb
tahphalat <phalat.tah@gmail.com>
2025-03-24 22:35:20 +0700

    vercel3

--- next.config.js
+++ next.config.js
@@ -19,6 +19,10 @@ const nextConfig = {
     },
     experimental: {
         serverActions: true
+    },
+    env:{
+        FRONTEND_URL: process.env.FRONTEND_URL,
+        BACKEND_URL: process.env.BACKEND_URL,
     }
 };
 
