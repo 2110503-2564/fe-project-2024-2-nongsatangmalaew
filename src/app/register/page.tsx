d3857a8bc2a02b521d4a3bd5f17fc25da2bab68e
tahphalat <phalat.tah@gmail.com>
2025-03-25 11:57:42 +0700

    del cat

--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -29,7 +29,7 @@ export default function RegisterForm() {
     e.preventDefault();
     setMessage("");
 
-    const res = await fetch("https://backend-for-fe-project.vercel.app/api/v1/auth/register", {
+    const res = await fetch("http://localhost:5000/api/v1/auth/register", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify(form),
@@ -102,4 +102,3 @@ export default function RegisterForm() {
     </div>
   );
 }
-
