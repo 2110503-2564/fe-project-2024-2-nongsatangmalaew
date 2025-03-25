f085d7936731bc2fcd912baa9b6037b8709a39d6
tahphalat <phalat.tah@gmail.com>
2025-03-25 10:11:04 +0700

    comment TravelCard

--- src/app/TravelCard.tsx
+++ src/app/TravelCard.tsx
@@ -1,35 +1,35 @@
-"use client";
-import { VlogPlayer } from "@/components/VlogPlayer";
-import { useState } from "react";
-import { Rating } from "@mui/material";
-import { useWindowListener } from "@/hooks/useWindowListener";
-export function TravelCard() {
-  const [playing, setPlaying] = useState(true);
-  const [rating, setRating] = useState(0);
-  const [pointerPosition, setPointerPosition] = useState({x:0, y:0});
+// "use client";
+// import { VlogPlayer } from "@/components/VlogPlayer";
+// import { useState } from "react";
+// import { Rating } from "@mui/material";
+// import { useWindowListener } from "@/hooks/useWindowListener";
+// export function TravelCard() {
+//   const [playing, setPlaying] = useState(true);
+//   const [rating, setRating] = useState(0);
+//   const [pointerPosition, setPointerPosition] = useState({x:0, y:0});
   
-    useWindowListener('pointermove', (e)=>{
-        setPointerPosition({x:(e as PointerEvent).clientX , y:(e as PointerEvent).clientY})
-    })
+//     useWindowListener('pointermove', (e)=>{
+//         setPointerPosition({x:(e as PointerEvent).clientX , y:(e as PointerEvent).clientY})
+//     })
 
-  return (
-    <div className="w-[80%] shadow-lg mx-[10%] my-10 p-2 rounded-lg bg-gray-200 flex flex-row">
-      <VlogPlayer
-        vdoSrc="/video/polozhenie.mp4"
-        isPlaying={playing}
-      ></VlogPlayer>
-      <div className="m-5">
-        Car Demonstration
-        <button
-          className="block rounded-md bg-sky-600 hover:bg-indigo-600 px-3 py-2 text-white shadow-sm"
-          name="Book Car"
-          onClick={() => {
-            setPlaying(!playing);
-          }}
-        >
-          {playing ? "Pause" : "Play"}
-        </button>
-      </div>
-    </div>
-  );
-}
+//   return (
+//     <div className="w-[80%] shadow-lg mx-[10%] my-10 p-2 rounded-lg bg-gray-200 flex flex-row">
+//       <VlogPlayer
+//         vdoSrc="/video/polozhenie.mp4"
+//         isPlaying={playing}
+//       ></VlogPlayer>
+//       <div className="m-5">
+//         Car Demonstration
+//         <button
+//           className="block rounded-md bg-sky-600 hover:bg-indigo-600 px-3 py-2 text-white shadow-sm"
+//           name="Book Car"
+//           onClick={() => {
+//             setPlaying(!playing);
+//           }}
+//         >
+//           {playing ? "Pause" : "Play"}
+//         </button>
+//       </div>
+//     </div>
+//   );
+// }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,12 @@
 import Banner from "@/components/Banner";
 import Footer from "@/components/Footer";
-import { TravelCard } from "./TravelCard";
+// import { TravelCard } from "./TravelCard";
 
 export default function Home() {
   return (
     <main>
       <Banner />
-      <TravelCard></TravelCard>
+      {/* <TravelCard></TravelCard> */}
     </main>
   );
 }
